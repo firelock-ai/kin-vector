dea8e5d814f52d1d5a43092098fec5868093cbfe
Troy Fortin <troy@firelock.ai>
2026-03-28 05:33:19 -0400

    chore: remove tracked target/ directory
    Signed-off-by: Troy Fortin <troy@firelock.ai>

--- target/debug/build/getrandom-7953afb569040b4a/build_script_build-7953afb569040b4a.d
+++
@@ -1,5 +0,0 @@
-/Users/troyfortinjr/GitHub/kin-ecosystem/kin-vector/target/debug/build/getrandom-7953afb569040b4a/build_script_build-7953afb569040b4a.d: /Users/troyfortinjr/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/getrandom-0.4.2/build.rs
-
-/Users/troyfortinjr/GitHub/kin-ecosystem/kin-vector/target/debug/build/getrandom-7953afb569040b4a/build_script_build-7953afb569040b4a: /Users/troyfortinjr/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/getrandom-0.4.2/build.rs
-
-/Users/troyfortinjr/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/getrandom-0.4.2/build.rs:
--- target/debug/deps/kin_vector-14822068cdac4ddc.d
+++
@@ -1,5 +0,0 @@
-/Users/troyfortinjr/GitHub/kin-ecosystem/kin-vector/target/debug/deps/kin_vector-14822068cdac4ddc.d: src/lib.rs
-
-/Users/troyfortinjr/GitHub/kin-ecosystem/kin-vector/target/debug/deps/kin_vector-14822068cdac4ddc: src/lib.rs
-
-src/lib.rs:
